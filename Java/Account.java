09c141284d110c2b1e97bfef8affdf8160b0eeb9
Anonimo <software.newspace@gmail.com>
2021-10-21 18:39:02 -0500

    Java

---
+++ Java/Account.java
@@ -0,0 +1,7 @@
+class Account {
+    Integer id;
+    String name;
+    String document;
+    String emial;
+    String password;
+}
---
+++ Java/Main.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String[] args) {
+        system.out.println("Hola mundo");
+    }
+}
