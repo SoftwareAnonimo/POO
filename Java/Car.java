aa87787780febbbbde6f0da348bcbc6b10f511db
Anonimo <software.newspace@gmail.com>
2021-10-22 15:39:04 -0500

    Ejemplo 1 JAVA

--- Java/Car.java
+++ Java/Car.java
@@ -3,4 +3,8 @@ class Car {
     String license;
     String driver;
     Integer passegengers;
+    
+    void printDataCar() {
+        System.out.println("License: " + license + " Driver: " + driver);
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,17 @@
 class Main {
     public static void main(String[] args) {
-        system.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passegengers = 4;
+        car.printDataCar();
+        
+        Car car2 = new Car();
+        car2.license = "GWJ820";
+        car2.driver = "Andrea Miller";
+        car2.passegengers = 3;
+        car2.printDataCar();
     }
 }
