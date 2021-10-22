e32e745b7e42526c04deb1332eee4ff7108da49b
Anonimo <software.newspace@gmail.com>
2021-10-22 17:12:28 -0500

    Ejemeplo 1 JAVA

--- Java/Account.java
+++ Java/Account.java
@@ -2,6 +2,11 @@ class Account {
     Integer id;
     String name;
     String document;
-    String emial;
+    String email;
     String password;
+
+    public Account(String name, String document) {
+        this.name = name;
+        this.document = document;
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -2,15 +2,11 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
 
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND452"));
         car.passegengers = 4;
         car.printDataCar();
         
-        Car car2 = new Car();
-        car2.license = "GWJ820";
-        car2.driver = "Andrea Miller";
+        Car car2 = new Car("HYM792", new Account("Andrea Miller", "TSW058"));
         car2.passegengers = 3;
         car2.printDataCar();
     }
