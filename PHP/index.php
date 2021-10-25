3650ff64d32b6c4609e31080bb2737123b004a13
Anonimo <software.newspace@gmail.com>
2021-10-25 18:31:17 -0500

    Php

--- PHP/index.php
+++ PHP/index.php
@@ -4,7 +4,7 @@ require_once("uberX.php");
 require_once("uberPool.php");
 require_once("account.php");
 
-$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX = new UberX("YDB857", new Account("Andres Herrera", "OCE837"), "Chevrolet", "Spark");
 $uberX->printDataCar();
 
 $uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
