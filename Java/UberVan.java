838af6af23da265f4937ebdbf671dba26804f0bd
Anonimo <software.newspace@gmail.com>
2021-10-25 15:39:04 -0500

    Java

---
+++ Java/UberVan.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+class UberVan extends Car {
+    Map<String, Map<String,Integer>> typeCarAccepted;
+    ArrayList<String> seatMaterial;
+
+    public UberVan(String license, Account driver,
+     Map<String, Map<String,Integer>> typeCarAccepted,
+     ArrayList<String> seatMaterial){
+        super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatMaterial = seatMaterial;
+    }
+}
