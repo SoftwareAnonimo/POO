8b6e0e56c8737c36147334eca567807f5a0c0c05
Anonimo <software.newspace@gmail.com>
2021-10-25 15:38:42 -0500

    Js

--- Js/Car.js
+++ Js/Car.js
@@ -1,6 +1,12 @@
-function Car() {
+function Car(license, drive) {
     this.id;
-    this.license;
-    this.driver;
+    this.license = license;
+    this.driver = drive;
     this.passenger;
 };
+
+Car.prototype.printDataCar = function() {
+    console.log(this.driver)
+    console.log(this.driver.name)
+    console.log(this.driver.document)
+}
---
+++ Js/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AWQ609", new Account("Andres Herrera", "WFTR2529"))
+car.passenger = 4;
+car.printDataCar();
